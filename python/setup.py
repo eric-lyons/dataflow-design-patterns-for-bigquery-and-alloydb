5e3d10d6a5bae347c2dea16734eab3c8a91034f7
Eric Lyons <53839634+eric-lyons@users.noreply.github.com>
2022-12-22 17:08:28 +0000

    Add files via upload

---
+++ python/setup.py
@@ -0,0 +1,15 @@
+
+import setuptools
+
+setuptools.setup(
+    name='beam-flex-demo',
+    version='1.0',
+    install_requires=[],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+
+    # warnings suppressors
+    description='Beam Flex Demo',
+    author='EGLyons',
+    author_email='NA',
+    url='',)
